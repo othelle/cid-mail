186f6d9dc506b0cecc8a435da3c0e284b1d83258
Vasily Vlasov <vasily.vlasov@gmail.com>
2012-07-23 00:06:37 +0400

    Added LogController stub

---
+++ grails-app/controllers/com/othelle/cig/email/LogController.groovy
@@ -0,0 +1,13 @@
+package com.othelle.cig.email
+
+class LogController {
+
+    def index() {
+        //Display all the log files
+    }
+
+    def show() {
+        //Display the content of the file
+        def fileNameToDisplay = params.fileName
+    }
+}
